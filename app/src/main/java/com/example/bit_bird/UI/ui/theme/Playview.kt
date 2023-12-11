800c9173d9adc6679a19b7fc54e9d49bbd8a0c02
Andy4433 <anderson.s.brito613@gmail.com>
2023-12-11 12:49:02 -0300

    feat: criando as funcionalindades do jogo: background, fps

--- app/src/main/java/com/example/bit_bird/UI/ui/theme/Playview.kt
+++ app/src/main/java/com/example/bit_bird/UI/ui/theme/Playview.kt
@@ -3,17 +3,46 @@ package com.example.bit_bird.UI.ui.theme
 import android.content.Context
 import android.view.SurfaceHolder
 import android.view.SurfaceView
+import com.example.bit_bird.thread.Playthread
 
 class Playview(context: Context?) : SurfaceView(context), SurfaceHolder.Callback {
+
+    private val TAG = "PlayView"
+    private var playthread : Playthread? = null
+
+
+    init{
+        val holder = holder
+        holder.addCallback(this)
+        isFocusable = true
+        playthread = Playthread(holder, resources)
+    }
     override fun surfaceCreated(p0: SurfaceHolder) {
-        TODO("Not yet implemented")
+        if(!playthread!!.isRunning){
+            playthread= p0.let{
+                Playthread(it!!, resources)
+            }
+            }else{
+                playthread!!.start()
+            }
+        }
     }
 
     override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
-        TODO("Not yet implemented")
+
     }
 
     override fun surfaceDestroyed(p0: SurfaceHolder) {
-        TODO("Not yet implemented")
+        if (playthread!!.isRunning){
+            playthread.isRunning = false
+            var isCheck: Boolean = true
+            while (isCheck){
+                try {
+                    playthread!!.join()
+                }catch (e: InterruptedException){
+
+                }
+            }
+        }
     }
 }
