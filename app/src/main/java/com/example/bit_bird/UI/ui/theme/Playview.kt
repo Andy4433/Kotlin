180f3c75a16cbc43b9df9a37165975fee853d135
Andy4433 <anderson.s.brito613@gmail.com>
2023-12-11 10:28:53 -0300

    novas funçoes

---
+++ app/src/main/java/com/example/bit_bird/UI/ui/theme/Playview.kt
@@ -0,0 +1,19 @@
+package com.example.bit_bird.UI.ui.theme
+
+import android.content.Context
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+
+class Playview(context: Context?) : SurfaceView(context), SurfaceHolder.Callback {
+    override fun surfaceCreated(p0: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder) {
+        TODO("Not yet implemented")
+    }
+}
