8b4275d731b0504125430b28b2f76d53e9213a8c
Andy4433 <anderson.s.brito613@gmail.com>
2023-12-11 18:09:06 -0300

    feat:mudanças no activity e arrumando erros

--- app/src/main/java/com/example/bit_bird/MainActivity.kt
+++ app/src/main/java/com/example/bit_bird/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.bit_bird
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private val Tag="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnPlay.setOnClickListener{
+            Log.d(Tag,"Button play clicked")
+        }
     }
 }
